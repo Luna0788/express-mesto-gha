a2301a1837fc30f4e788c81b640c71708e93a3bf
Yuliya S <safronova.yuv@gmail.com>
2023-11-30 02:41:05 +0400

    fix: add jwt to cookie

--- controllers/users.js
+++ controllers/users.js
@@ -147,7 +147,7 @@ module.exports.login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
-      res.send({ token });
+      res.cookie('jwt', token, { maxAge: 3600000, httpOnly: true });
     })
     .catch(next);
 };
